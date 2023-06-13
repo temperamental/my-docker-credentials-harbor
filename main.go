d759328b0d196507e299517a8285f05cba4f53b5
Martyn Leadley <Martyn.Leadley@team.telstra.com>
2023-06-14 10:24:38 +1200

    Keeping Safe

--- main.go
+++ main.go
@@ -1,39 +1,46 @@
 package main
 
+// For reference
+// * https://github.com/awslabs/amazon-ecr-credential-helper/blob/main/ecr-login/ecr.go
+// * https://github.com/Azure/acr-docker-credential-helper/
+
 import (
-    "fmt"
-    "github.com/docker/docker/api/types/credentials"
-    "github.com/docker/docker/client"
-    "os"
+	"fmt"
+	"os"
+
+	"github.com/moby/moby/api/types/credentials"
+	"github.com/moby/moby/client"
 )
 
 func main() {
-    // Get the Docker client.
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	// Get the Docker client.
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    // Get the credentials from the environment.
-    username := os.Getenv("HARBOR_CLIENT_NAME")
-    password := os.Getenv("HARBOR_CLIENT_SECRET")
+	// Get the credentials from the environment.
+	username := os.Getenv("HARBOR_CLIENT_NAME")
+	password := os.Getenv("HARBOR_CLIENT_SECRET")
 
-    // Create the credentials object.
-    credentials := &credentials.Credentials{
-        ServerAddress: "https://index.docker.io/v1/",
-        Username:     username,
-        Password:     password,
-    }
+	// TODO: This seems specific to authenticating to hub.docker.io
+	// Need to update to be Harbor specific
 
-    // Store the credentials in the Docker daemon.
-    err = cli.StoreCredentials(credentials)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	// Create the credentials object.
+	credentials := &credentials.Credentials{
+		ServerAddress: "https://index.docker.io/v1/",
+		Username:      username,
+		Password:      password,
+	}
 
-    // Print a success message.
-    fmt.Println("Credentials stored successfully!")
+	// Store the credentials in the Docker daemon.
+	err = cli.StoreCredentials(credentials)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Print a success message.
+	fmt.Println("Credentials stored successfully!")
 }
-
